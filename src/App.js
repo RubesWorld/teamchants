b82699880b345ac8af013bec03ff9e74202c40e8
RubesWorld <ra15@outlook.com>
2022-03-08 21:04:12 -0500

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>TeamChants Header</p>
       </header>
+      <main className="body"></main>
+      <div className="footer"> Footer</div>
     </div>
   );
 }
